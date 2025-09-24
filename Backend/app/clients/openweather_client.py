dbcc75e5aed143097d7e824bceca89003e75fe78
Olexsandr Zoshchuk <kutabare.p@gmail.com>
2025-09-24 21:20:21 +0300

    Added comments to openmeteo_client + openweather_client

--- Backend/app/clients/openweather_client.py
+++ Backend/app/clients/openweather_client.py
@@ -1,2 +1,6 @@
 class OpenWeatherClient:
+    # TODO: Make function which can get any params which are needed
+    # TODO: Get API key from .env
+    # TODO: Get Data from key
+    # TODO: Remove unnecessary data
     pass
