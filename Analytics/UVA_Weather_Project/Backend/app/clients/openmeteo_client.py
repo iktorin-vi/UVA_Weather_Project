d107bad85b31546fae490456b772637cce8b4496
Olexsandr Zoshchuk <kutabare.p@gmail.com>
2025-09-28 20:45:24 +0300

    removed unnecessary files

--- Analytics/UVA_Weather_Project/Backend/app/clients/openmeteo_client.py
+++
@@ -1,6 +0,0 @@
-class OpenMeteoClient:
-    # TODO: Make function which can get any params which are needed
-    # TODO: Get API key from .env
-    # TODO: Get Data from key
-    # TODO: Remove unnecessary data
-    pass
